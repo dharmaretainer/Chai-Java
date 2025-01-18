cc29eb5ec010ff9383b13a935cf57726a31cb370
Aryan Karewar <127008217+dharmaretainer@users.noreply.github.com>
2025-01-18 11:25:48 +0000

    datatypes

---
+++ .devcontainer/01_Basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 144553
+let accountEmail ="aryan@google.com"
+var accountPassword="12345"
+accountCity="jaipur"
+// accountId=2
+console.log(accountId);
+console.table([accountPassword , accountEmail,accountCity]);
---
+++ .devcontainer/01_Basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";//treat all JS code as newer version
+// alert(3+3)//we are using nodejs, not browser
+console.log(3+3) 
+console.log("HItesh")
+let name="aryan"
+let age =20
+let isLoggedIn=false
+//number=>2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//mull=>standalone value
+//undefined=>
+//symbol=>uniqueness
+
+
+
+//object
+console.log(typeof age);
