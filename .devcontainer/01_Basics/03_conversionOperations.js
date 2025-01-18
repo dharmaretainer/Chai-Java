3bfae0f35656c98b665c9879f2280891b00e6091
Aryan Karewar <127008217+dharmaretainer@users.noreply.github.com>
2025-01-18 11:43:53 +0000

    conversionofdatatypes

---
+++ .devcontainer/01_Basics/03_conversionOperations.js
@@ -0,0 +1,20 @@
+let score="33"
+console.log(typeof score)
+
+let valueInNumber =Number(score)//converts in number datatype
+console.log(typeof valueInNumber)
+
+//"33"=>33
+//"33abc=>NaN"
+//true=>1;false=>0
+
+let isLoggedIn="aryan"
+let booleanIsLoggedIn=Boolean(isLoggedIn)//converts in boolean
+console.log(booleanIsLoggedIn);
+//1=>true//0=>false
+//""=>false
+//"aryan=>true"
+
+let someNumber=33
+let stringNumber =String(someNumber)//converts in string
+console.log(typeof stringNumber)
