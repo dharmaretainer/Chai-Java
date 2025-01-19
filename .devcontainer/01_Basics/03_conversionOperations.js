88c06b3da9a78736ac7de09bbeefa235d36fb44b
Aryan Karewar <127008217+dharmaretainer@users.noreply.github.com>
2025-01-19 06:25:43 +0000

    conversionoperations

--- .devcontainer/01_Basics/03_conversionOperations.js
+++ .devcontainer/01_Basics/03_conversionOperations.js
@@ -18,3 +18,24 @@ console.log(booleanIsLoggedIn);
 let someNumber=33
 let stringNumber =String(someNumber)//converts in string
 console.log(typeof stringNumber)
+//*****************Operations**********//
+let value =3
+let negValue=-value
+console.log(negValue);
+
+let str1="hello"
+let str2=" aryan"
+let str3 = str1+str2
+console.log(str3)
+
+console.log("1"+2)//12
+console.log(1+"2")//12
+console.log("1"+2+2)//122
+console.log(1+2+"2")//32
+
+console.log(+true)//1
+console.log(+"")//0
+let prefix=100
+console.log(prefix++)//101
+console.log(++prefix)//101
+
