5a6c10aec8b2ddc9fb4bca479e5d8dbbf9760cab
Aryan Karewar <127008217+dharmaretainer@users.noreply.github.com>
2025-01-19 06:37:50 +0000

    comparisons

---
+++ .devcontainer/01_Basics/04_comparisons.js
@@ -0,0 +1,17 @@
+//console.log(2>1)
+//console.log(2<1)
+//console.log(2==1)
+//console.log(2!=1)
+
+//console.log("2">1)true , converts string to number automatically
+//console.log("02">1)true 
+
+console.log(null>0);//false
+console.log(null ==0);//false
+console.log(null>=0);//true null converts to zero when < > operator is there
+
+console.log(undefined>0)//false
+console.log(undefined==0)//false
+console.log(undefined<0)//false
+// === strict check , checks value and datatype both
+console.log("2"===2)//false
