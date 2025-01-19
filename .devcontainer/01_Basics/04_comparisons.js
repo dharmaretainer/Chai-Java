11bae99e44b926de8e4de4909edf05f6b3296095
Aryan Karewar <127008217+dharmaretainer@users.noreply.github.com>
2025-01-19 13:42:03 +0000

    comparisons

--- .devcontainer/01_Basics/04_comparisons.js
+++ .devcontainer/01_Basics/04_comparisons.js
@@ -15,3 +15,21 @@ console.log(undefined==0)//false
 console.log(undefined<0)//false
 // === strict check , checks value and datatype both
 console.log("2"===2)//false
+
+
+
+// stack (primitive ), heap(non-primitive)
+let myYoutubeName="Aryanlearns"
+let anothername=myYoutubeName
+anothername="chaiaurcode"
+console.log(anothername)
+console.log(myYoutubeName)
+
+let userOne={
+    email:"usergoogle.com",
+    upi:"user"
+}
+let userTwo=userOne
+userTwo.email="aryan@gmail.com"
+console.log(userOne.email)
+console.log(userTwo.email)//both changed
