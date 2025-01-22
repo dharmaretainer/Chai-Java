201ba3a8a698bc39bba05b63c4adc8bb5281b7cd
Aryan Karewar <127008217+dharmaretainer@users.noreply.github.com>
2025-01-22 06:54:26 +0000

    strings

---
+++ .devcontainer/01_Basics/05_strings.js
@@ -0,0 +1,10 @@
+const name ="Aryan";
+const age =20;
+const city = new String("Parbhani");
+//concatenation
+console.log(name +age+"sahaj");
+//newer method is 
+console.log(`my name is ${name} and i am ${age} years old and i live in ${city}. ` );
+//Strings methods
+console.log(name.toUpperCase()+" "+ city.charAt(1)+" "+city.indexOf('n'));
+//learn more methods
