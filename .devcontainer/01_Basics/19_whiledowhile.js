32be293328930bda98533e96d232ccab7d209022
Aryan Karewar <127008217+dharmaretainer@users.noreply.github.com>
2025-03-06 18:18:25 +0000

    whiledowhile

---
+++ .devcontainer/01_Basics/19_whiledowhile.js
@@ -0,0 +1,14 @@
+//while
+let index=0
+while(index <=10){
+    console.log(`Value of index is ${index}`);
+    index=index+2;
+     
+}
+let score=11
+do{
+    console.log(`score is ${score}`);
+    score++
+
+}
+while(score <=10);//kaam pehle hota hai condition badme check hoti hai , atleast ek baar toh output dega 
