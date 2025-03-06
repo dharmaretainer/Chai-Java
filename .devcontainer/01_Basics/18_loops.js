56a6c0dd576dc9429a542773bd0ab925d4a3df4a
Aryan Karewar <127008217+dharmaretainer@users.noreply.github.com>
2025-03-06 18:09:26 +0000

    loops

---
+++ .devcontainer/01_Basics/18_loops.js
@@ -0,0 +1,18 @@
+//for 
+// for(let index=0; index<10;index++){
+//     const element =index;
+//     console.log(index);
+// }
+
+//break and continue 
+
+for(let i=1; i<=20;i++){
+    if(i==5){
+        console.log("Detected 5");
+        // break //it breaks the loop
+        continue // skips 5 and continues the loop
+    }
+    console.log(`Value of i is ${i}`);
+    
+    
+}
